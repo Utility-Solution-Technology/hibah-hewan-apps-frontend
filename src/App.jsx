2b5f799c98ce9baf9e122a81b6f102a0207afb47
Ibrahimyunel <ibrahim.cbshp@gmail.com>
2023-05-27 12:46:12 +0700

    create login form

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NoPage from './pages/NoPage';
 import Navbar from './Navbar';
 import Home from './pages/Home';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Navbar />}>
           <Route index element={<Home />} />
           <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
