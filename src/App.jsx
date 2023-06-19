a6b8983e6bb68bd43a984c6f3239bbe094b37f9c
annafkh <annafi101@gmail.com>
2023-06-19 21:56:11 +0700

    profile update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NoPage from './pages/NoPage';
+import Profile from './pages/Profile';
 import Navbar from './Navbar';
 import Home from './pages/Home';
 import Register from './pages/Register';
@@ -18,7 +18,7 @@ function App() {
           <Route path="register" element={<Register />} />
           <Route path="login" element={<Login />} />
           <Route path="hibah-hewan" element={<HibahkanHewan />} />
-          <Route path="*" element={<NoPage />} />
+          <Route path="*" element={<Profile />} />
           <Route path="detail" element={<Detail />} />
           <Route path="favorite" element={<Favorite />} />
 
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import '../style/profile.scss';
+import Footer from '../components/Footer';
+
+function Profile() {
+  return (
+    <>
+      <main>
+        <article>
+          <h1 className="ok">Profil</h1>
+          <form className="C-d-flex">
+            <div className="col-lg-6">
+              <div className="mb-3">
+                <label className="form-label" htmlFor="name">Nama</label>
+                <input
+                  readOnly
+                  type="text"
+                  className="form-control"
+                  id="name"
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label" htmlFor="email">Email</label>
+                <input
+                  readOnly
+                  type="text"
+                  className="form-control"
+                  id="email"
+                />
+              </div>
+              <div className="row mb-3">
+                <div className="col-6">
+                  <label className="form-label" htmlFor="whatsapp">WhatsApp</label>
+                  <input
+                    readOnly
+                    type="number"
+                    className="form-control"
+                    id="whatsApp"
+                  />
+                </div>
+              </div>
+            </div>
+          </form>
+        </article>
+      </main>
+      &nbsp;
+      <Footer />
+    </>
+  );
+}
+
+export default Profile;
