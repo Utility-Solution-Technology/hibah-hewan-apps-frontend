3b3bdf6060b073b9a46e32f752c12fdca522e64c
Ibrahimyunel <ibrahim.cbshp@gmail.com>
2023-06-19 17:33:02 +0700

    create api request for animal list

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,15 +5,15 @@ function Footer() {
   return (
     // eslint-disable-next-line react/react-in-jsx-scope
     <div className="footer">
-      <Navbar className="mt-5" bg="dark" variant="dark">
+      <Navbar className="mt-5 py-5" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Abule</Navbar.Brand>
           <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text>
-              Copyright HumbleDev @2023: <a href="#login" className="text-decoration-none">All right reserved</a>
-            </Navbar.Text>
-          </Navbar.Collapse>
+          <div className="d-block text-center d-md-flex justify-content-between align-items-center w-100">
+            <div>
+              <Navbar.Brand href="/">Abule</Navbar.Brand>
+            </div>
+            <h6 className="text-secondary m-0">Copyright HumbleDev @2023: <a href="#login" className="text-decoration-none">All right reserved</a></h6>
+          </div>
         </Container>
       </Navbar>
     </div>
