4a130aeec3daed2f9c0f9b04805f2b6d0d278693
Ibrahimyunel <ibrahim.cbshp@gmail.com>
2023-06-19 17:33:02 +0700

    create api request for animal list

--- src/components/home/CardContent.jsx
+++ src/components/home/CardContent.jsx
@@ -1,9 +1,22 @@
-/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
-// import imghewan from '../../../public/images/hewan/hewan1.png'
+import { useDispatch } from 'react-redux';
+import axios from '../../proxy/baseURL';
+import { setErrFetchList } from '../../redux/upload-slice';
+import Animal from '../Animal';
 
 function CardContent() {
+  const urlGetUpload = 'https://raw.githubusercontent.com/Utility-Solution-Technology/hibah-hewan-apps-backend/master/images/upload/';
+  const dispatch = useDispatch();
+
+  const getImageUpload = async (uploadResponse) => {
+    try {
+      const res = await axios.post('/latest-uploads');
+      res.data.getUpload.map((item) => uploadResponse.push(item));
+    } catch {
+      dispatch(setErrFetchList());
+    }
+  };
+
   return (
     <div>
       <section className="product mt-5 section-margin">
@@ -16,61 +29,11 @@ function CardContent() {
           </div>
         </div>
       </section>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-3">
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src={`${process.env.PUBLIC_URL}/images/hewan/hewan1.png`} />
-              <Card.Body>
-                <Card.Title>Anabul Anjing</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col-md-3">
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src={`${process.env.PUBLIC_URL}/images/hewan/hewan1.png`} />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col-md-3">
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src={`${process.env.PUBLIC_URL}/images/hewan/hewan1.png`} />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col-md-3">
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src={`${process.env.PUBLIC_URL}/images/hewan/hewan1.png`} />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div>
-        </div>
+      <div className="C-animal-wrapper container">
+        <Animal
+          urlGetUpload={urlGetUpload}
+          getImageUpload={getImageUpload}
+        />
       </div>
     </div>
   );
